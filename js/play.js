52ca148d2372304f20b8bb6f52233005e9d59a56
LameLemon <garybsantos@gmail.com>
2018-02-12 18:03:17 -0500

    Skeleton code and game

---
+++ js/play.js
@@ -0,0 +1,16 @@
+var playState = {
+  // Global variables declaration
+
+  // Instantiate and assign game objects
+  create: function() {
+
+  },
+
+  // Anything that needs to be checked
+  // Collisions, user input etc...
+  update: function() {
+
+  }
+};
+
+// Helper functions go below

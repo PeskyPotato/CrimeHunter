50b97fc11e105b2da0e9aa2c63c2697196a5f7f8
LameLemon <garybsantos@gmail.com>
2018-02-12 18:03:17 -0500

    Skeleton code and game

---
+++ js/game.js
@@ -0,0 +1,11 @@
+var game = new Phaser.Game(480, 360, null, 'gameDiv');
+
+// add each game state
+
+game.state.add('boot', bootState);
+game.state.add('load', loadState);
+game.state.add('title', titleState);
+game.state.add('play', playState);
+
+// call the boot boot
+game.state.start('boot');

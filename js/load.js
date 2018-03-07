e9f46e830c4d5245a78fc5532dfefd34a336254d
LameLemon <garybsantos@gmail.com>
2018-03-07 13:31:16 -0500

    Scoreboard added

--- js/load.js
+++ js/load.js
@@ -13,6 +13,9 @@ var loadState={
     game.load.image('ultskill', 'assets/ultskill.png');
     game.load.image('addhealth', 'assets/health.png');
     game.load.spritesheet('characters', 'assets/Spritesheet.png', 38, 64);
+    game.load.image('playButton', 'assets/buttons/play.png');
+    game.load.image('scoreButton', 'assets/buttons/score.png');
+    game.load.image('backButton', 'assets/buttons/back.png');
 
     game.load.tilemap('level', 'assets/map/Level1.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles', 'assets/map/TilesetRoad1.png');
